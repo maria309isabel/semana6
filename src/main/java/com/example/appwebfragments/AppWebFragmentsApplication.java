a761ccfc9293e6057c9e3549d7aedb5e7a22beca
maria309isabel <isa.mar.xv@gmail.com>
2023-10-01 09:46:59 -0500

    mensaje

---
+++ src/main/java/com/example/appwebfragments/AppWebFragmentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.appwebfragments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebFragmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebFragmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/appwebfragments/AppWebFragmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appwebfragments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebFragmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
