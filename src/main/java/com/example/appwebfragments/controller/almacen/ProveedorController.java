8287d6551e45c2df06ac166c18be614bf2ad98b9
maria309isabel <isa.mar.xv@gmail.com>
2023-10-01 11:48:59 -0500

    Fin

---
+++ src/main/java/com/example/appwebfragments/controller/almacen/ProveedorController.java
@@ -0,0 +1,15 @@
+package com.example.appwebfragments.controller.almacen;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/almacen/proveedor")
+public class ProveedorController {
+    @GetMapping("")
+    public String index(){
+
+        return "almacen/proveedor";
+    }
+}
